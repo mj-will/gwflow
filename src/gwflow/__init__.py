eb9fe772af9c927ea92bf15ab941abedc4e6ccd6
Michael J. Williams <michaeljw1@googlemail.com>
2025-09-12 09:49:28 +0100

    ENH: add initial package

---
+++ src/gwflow/__init__.py
@@ -0,0 +1,18 @@
+"""
+gwflow: Normalizing flows for gravitational-wave inference
+"""
+import logging
+from importlib.metadata import PackageNotFoundError, version
+
+from .flow import GWCalFlow
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = [
+    "GWCalFlow",
+]
